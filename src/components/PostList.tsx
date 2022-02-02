ccf6b5143bfa7bf7a9da24ef71f9d4e0f8cf5e42
Yury <yurydgfuentes@gmail.com>
2022-02-02 22:14:22 +0200

    use custom hook to populate post info

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,10 +1,19 @@
-type PostListProps = {};
+import { usePosts } from "../hooks";
+import { PostItem } from "./PostItem";
 
-export const PostList: React.FC<PostListProps> = (props) => {
-    console.log({ props });
+export const PostList: React.FC = () => {
+    const { data, loading, error } = usePosts();
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :( {error.message}</p>;
+
     return (
-        <div>
-            <h2>PostList works!</h2>
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    {data?.allPosts.map(item => <PostItem key={item.id} post={item} />)}
+                </div>
+            </div>
+        </section>
     )
 }
