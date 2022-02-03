93610f82f438e4ab2c9b0c14a347dcd9ac959f43
Yury <yurydgfuentes@gmail.com>
2022-02-03 11:20:59 +0200

    v3: add query to get post

--- src/Queries/index.ts
+++ src/Queries/index.ts
@@ -2,20 +2,41 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_POSTS = gql`
 query allPosts($filter: PostFilter) {
-    allPosts(filter: $filter) {
+  allPosts(filter: $filter) {
+    id
+    title
+    User {
       id
-      title
-      User {
-        id
-        name
-      }
-      views
-      Comments {
-        id
-        post_id
-        body
-        date
-      }
+      name
+    }
+    views
+    Comments {
+      id
+      post_id
+      body
+      date
     }
   }
+}
+`;
+
+export const GET_POST = gql`
+query Post($postId: ID!) {
+  Post(id: $postId) {
+    id
+    title
+    views
+    user_id
+    User {
+      id
+      name
+    }
+    Comments {
+      id
+      post_id
+      body
+      date
+    }
+  }
+}
 `;
